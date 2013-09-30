df52d688ba63fffe6ac164e55fcd042a7689fff4
hiroshima <iga1128@gmail.com>
2013-09-30 18:19:59 +0900

    html->php

--- link.php
+++ link.php
@@ -27,51 +27,47 @@
 <body>
 <div id="wrapper">
 	<div id="header">
-		<h1><strong>TOEIC</strong>950点(<strong>リスニグ満点</strong>)わすれっぽいエンジニアのHP</h1>
+		<?php include"header.html"; ?>
 	</div>
 	<div id="menu" >
-		<ul id="dropMenu">
-			<li><a href="index.html">Home</a></li>
-			<li><a href="http://toeicengineer.blogspot.jp/" target="_blank">Blog</a></li>
-			<li><a href="link.html" >Link</a></li>
-		</ul>
+		<?php include"menu.html";  ?>
 	</div>
 	<div id="box1" class="box">
-		<h2 class="headline">英語の動画ニュース</h2>
+		<h2 class="headline">News</h2>
 		<ul>
 			<li><a href="http://www3.nhk.or.jp/nhkworld/" target="_blank" title="NHKニュース！">NHKWORLD (ENGLISH)</a></li>
 			<li><a href="http://edition.cnn.com/video/" target="_blank" title="CNNニュース！">CNN VIDEO</a></li>
+			<li><a href="http://bizmakoto.jp/bizid/" target="_blank" title="誠">誠</a></li>
+			<li><a href="http://www.yomiuri.co.jp/" target="_blank" title="読売">読売オンライン</a></li>
+			<li><a href="http://www.nikkei.com/" target="_blank" title="日系">日経新聞</a></li>
+			
 		</ul>
 	</div>
 	<div id="box2" class="box">
-		<h2 class="headline">定番検索サイト</h2>
+		<h2 class="headline">Search engines</h2>
 		<ul>
-			<li>
-				<a href="http://www.google.co.jp/webhp?rls=ig" target="_blank">Google</a>
-			</li>
-			<li>
-				<a href="http://www.yahoo.co.jp/" target="_blank">Yahoo Japan</a>
-			</li>
-			<li>
-				<a href="http://jp.msn.com/" target="_blank">Msn Japan</a>
-			</li>
+			<li><a href="http://www.google.co.jp/webhp?rls=ig" target="_blank">Google</a></li>
+			<li><a href="http://www.yahoo.co.jp/" target="_blank">Yahoo Japan</a></li>
+			<li><a href="http://jp.msn.com/" target="_blank">Msn Japan</a></li>
 			<li><a href="http://www.livedoor.com/" target="_blank">Livedoor</a></li>
 			<li><a href="http://www.goo.ne.jp/" target="_blank">goo</a></li>
 			<li><a href="http://www.naver.jp/" target="_blank">NAVER</a></li>
 		</ul>
 	</div>
 	<div  id="box3" class="box">
-		<h2 class="headline">英語ニュース</h2>
+		<h2 class="headline">English news</h2>
 		<ul>
 			<li><a href="http://metronews.ca/" target="_blank">MetroNews</a></li>
 			<li><a href="http://ca.yahoo.com/" target="_blank">Yahoo Canada</a></li>
 			<li><a href="http://ca.msn.com/" target="_blank">Msn Canada</a></li>
 			<li><a href="http://www.wired.com/" target="_blank">wired.com</a></li>
-			
+			<li><a href="http://www.interviewmagazine.com/#music" target="_blank" >interview magagine</a></li>
+			<li><a href="http://ca.omg.yahoo.com/" target="_blank" >omg!</a></li>
+			<li><a href="http://www.alc.co.jp/eng/newsbiz/headline/" target="_blank" >やさしい英語でニュースを読む</a></li>
 		</ul>
 	</div>
 	<div  id="box4" class="box">
-		<h2 class="headline">よくみるサイト(IT)</h2>
+		<h2 class="headline">IT information sites</h2>
 		<ul>
 			<li><a href="http://www.atmarkit.co.jp/" target="_blank">@IT</a></li>
 			<li><a href="http://itpro.nikkeibp.co.jp/" target="_blank">IT Pro</a></li>
@@ -80,7 +76,7 @@
 		</ul>
 	</div>
 	<div  id="box5" class="box">
-		<h2 class="headline">SNSで友達いっぱいできるかな～</h2>
+		<h2 class="headline">SNS sites</h2>
 		<ul>
 			<li><a href="https://www.facebook.com/hiroshibook" target="_blank">FaceBook</a></li>
 			<li><a href="http://twitter.com/" target="_blank">Titter</a></li>
@@ -88,6 +84,7 @@
 			<li><a id="Lang8" href="http://lang-8.com/" target="_blank">Lang8</a></li>
 			<li><a href="http://www.ameba.jp/" target="_blank">ameba</a></li>
 			<li><a href="http://www.nicovideo.jp/" target="_blank">にこにこ動画</a></li>
+			<li><a href="http://www.flickr.com/" target="_blank">flicker</a></li>
 		</ul>
 	</div>
 	<div  id="box6" class="box">
@@ -102,22 +99,28 @@
 		</ul>
 	</div>
 	<div id="box7" class="box">
-		<h2 class="headline">時代はネットショッピングがお得なのか！</h2>
+		<h2 class="headline">Online shopping site</h2>
 		<ul>
 			<li><a href="http://www.amazon.co.jp/">amazon.co.jp</a></li>
 			<li><a href="http://www.rakuten.co.jp/" target="_blank">楽天</a></li>
+			<li><a href="http://shopping.yahoo.co.jp/" target="_blank">Yahoo ショッピング</a></li>
+			<li><a href="http://www.dena-ec.com/" target="_blank">Dena Shopping</a></li>
+			<li><a href="http://www.aeonshop.com/index.html" target="_blank">イオン　ショップ</a></li>
 		</ul>
 	</div>
 	<div id="box8" class="box">
-		<h2 class="headline">め～るでめいる。。。orz...</h2>
+		<h2 class="headline">Free mail</h2>
 		<ul>
+			<li><a href="http://www.freeml.com/" target="_blank">Freeml</a></li>
 			<li><a href="http://www.mail.com/int/" target="_blank">mail.com</a></li>
+			<li><a href="https://gmail.google.com/" target="_blank">Gmail</a></li>
+			
 		</ul>
 	</div>
 	<div id="box9" class="box">
-		<h2 class="headline">これでFree!?すごいソフトト!</h2>
+		<h2 class="headline">Free soft</h2>
 		<ul>
-			<li><a href="http://ja.poderosa.org/index.html">Poderosa(ポデローサ) </a></li>
+			<li><a href="http://ja.poderosa.org/index.html">Poderosa(ポデローサ)</a></li>
 			<li><a href="http://www.forest.impress.co.jp/library/software/cajamarcaeye/download_10807.html">カハマルカの瞳(簡易キャプチャレコーダー)</a></li>
 			<li><a href="http://freesoft-100.com/" target="_blank">free soft 100</a></li>
 		</ul>
@@ -130,38 +133,25 @@
 		</ul>
 	</div>
 	<div id="box11" class="box">
-		<h2 class="headline">わすれっぽいエンジニア的リンク</h2>
+		<h2 class="headline">Matome</h2>
 		<ul>
 			<li><a href="http://matome.naver.jp/odai/2131600405413155201" target="_blank">英英辞典まとめ</a></li>
 			<li><a href="http://matome.naver.jp/odai/2131471255391732701" target="_blank" >システム開発にお役立ちフリーソフトまとめてみました</a></li>
 			<li><a href="http://matome.naver.jp/odai/2131556784006652701" target="_blank" >英語の資格まとめてみました。</a></li>
-			<li><a href="http://matome.naver.jp/odai/2133595365976855601" target="_blank" >TOEICer Blogs!</a></li>
-			<li><a href="http://matome.naver.jp/odai/2130882659276553201" target="_blank" >コマンドライン操作まとめ</a></li>
-			<li><a href="http://homepagelecture.web.fc2.com/" target="_blank" >いまさらHTML,CSS入門講座</a></li>
-			<li><a href="http://hprakuraku.web.fc2.com/" target="_blank" >ホームページの作り方</a></li>
-		</ul>
-	</div>
-	<div id="box12" class="box">
-		<h2 class="headline">最近の注目サイト</h2>
-		<ul>
-			<li><a href="https://www.facebook.com/groups/479377392081229/" target="_blank" >新宿コミュニケーションクラブ</a></li>
-			<li><a href="http://jp.hao123.com/?tn=sft_hp_hao123_jp" target="_blank" >Hao123</a></li>
-			<li><a href="http://chosuke.rumix.jp/" target="_blank">ちょー助</a></li>
-			<li><a href="https://live.cybozu.co.jp/" target="_blank">サイボウズLive</a></li>
-			<li><a href="http://www.lancers.jp/" target="_blank">Lancers</a></li>
-			<li><a href="https://www.datadeliver.net/" target="_blank">データ便</a></li>
-			<li><a href="http://weblearningplaza.jst.go.jp/" target="_blank">Ｗｅｂラーニングプラザ</a></li>
-			<li><a href="http://www.lifehacker.com/">Lifehacker</a></li>
-			<li><a href="http://www.lifehacker.jp/">Lifehacker[日本語版]</a></li>
-			<li><a href="http://www.teglet.co.jp/">TEGLET</a></li>
-		
 		</ul>
 	</div>
 	<div id="box13" class="box">
-		<h2 class="headline">CoffeeScript</h2>
+		<h2 class="headline">Sites for Programmer</h2>
 		<ul>
+			<li><a href="http://www.openspc2.org/reibun/JavaScript_technique/" target="_blank" >JavaScript テクニックブック</a></li>
 			<li><a href="http://coffeescript.org/" target="_blank" >CoffeeScript本家</a></li>
 			<li><a href="http://itpro.nikkeibp.co.jp/article/COLUMN/20120620/404226/" target="_blank" >CoffeeScriptって何？</a></li>
+			<li><a href="http://phpmentors.jp/" target="_blank" >php メンターズ</a></li>
+			<li><a href="http://homepagelecture.web.fc2.com/" target="_blank" >いまさらHTML,CSS入門講座</a></li>
+			<li><a href="http://hprakuraku.web.fc2.com/" target="_blank" >ホームページの作り方</a></li>
+			<li><a href="http://matome.naver.jp/odai/2130882659276553201" target="_blank" >コマンドライン操作まとめ</a></li>
+			<li><a href="http://ascii.jp/elem/000/000/438/438206/" target="_blank" >jQuery UI サンプル</a></li>
+			<li><a href="http://matome.naver.jp/odai/2132851038557722801" target="_blank" >jQueryでTable</a></li>
 		</ul>
 	</div>
 	<div id="box14" class="box">
@@ -172,18 +162,34 @@
 		</ul>
 	</div>
 	<div id="box15" class="box">
-		<h2 class="headline">自作プログラム</h2>
+		<h2 class="headline">Personal Links</h2>
 		<ul>
+			<li><a href="http://iga1128.wix.com/toeicengineer" target="_blank">WIXでHTML５ホームページを楽々作成</a></li>
 			<li><a href="./prg/mailForm.html" target="_blank" >メールメモ</a></li>
+			<li><a href="http://matome.naver.jp/odai/2133595365976855601" target="_blank" >TOEICer Blogs!</a></li>
+			<li><a href="https://www.facebook.com/groups/479377392081229/" target="_blank" >新宿コミュニケーションクラブ</a></li>
+			<li><a href="http://chosuke.rumix.jp/" target="_blank">ちょー助</a></li>
+			<li><a href="http://www.lancers.jp/" target="_blank">Lancers</a></li>
+			<li><a href="https://www.datadeliver.net/" target="_blank">データ便</a></li>
+			<li><a href="http://weblearningplaza.jst.go.jp/" target="_blank">Ｗｅｂラーニングプラザ</a></li>
+			<li><a href="http://dotinstall.com/" target="_blank">どっといんすとーる</a></li>
 		</ul>
 	</div>
-	<div id="box16" class="box">
-		<h2 class="headline">jQuery</h2>
+	<div id="box12" class="box">
+		<h2 class="headline">Asorted Site by Toeic Engineer</h2>
 		<ul>
-			<li><a href="http://ascii.jp/elem/000/000/438/438206/" target="_blank" >jQuery UI サンプル</a></li>
+			<li><a href="http://store.jp/" target="_blank" >Store.jp</a></li>
+			<li><a href="http://zerostore.jp/" target="_blank" >zero store</a></li>
+			<li><a href="https://thebase.in/" target="_blank" >thebase</a></li>
+			<li><a href="http://www.gmt.ne.jp/onlineshop/" target="_blank" >ゼロ　ショップ</a></li>
+			<li><a href="http://liginc.co.jp/" target="_blank" >LIG</a></li>
+			<li><a href="http://jp.hao123.com/?tn=sft_hp_hao123_jp" target="_blank" >Hao123</a></li>
+			<li><a href="https://live.cybozu.co.jp/" target="_blank">サイボウズLive</a></li>
+			<li><a href="http://www.lifehacker.com/">Lifehacker</a></li>
+			<li><a href="http://www.lifehacker.jp/">Lifehacker[日本語版]</a></li>
+			<li><a href="http://www.teglet.co.jp/">TEGLET</a></li>
 		</ul>
 	</div>
-	
 	<!--
 	<div id="hiroshiTowa" class="box">
 			<table id="HiroshimaTable" border="1" >
@@ -193,7 +199,7 @@
 				</td>
 			</tr>
 			<tr>
-				<td>
+				<td class="center">
 					<div id="hiroshiTowaHonbun">リンクをクリックするとHiroshi,ma!?の生態が表示されます</div>
 				</td>
 			</tr>
@@ -221,8 +227,7 @@
 	</form>
 	-->
 	<div id="footer">
-	<p>Copyright 2013<strong>Hiroshi Igarashi</strong></p>
-	<a href="http://toeicengineer.blogspot.jp/" target="_blank">TOEIC950点(リスニング満点) わすれっぽいエンジニアブログです。</a>
+		<?php include"footer.html"; ?>
 	</div>
 </div>
 </body>
